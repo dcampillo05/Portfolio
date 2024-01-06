3a8d1350faab102cd54a821bf696c86a3d635681
Daniel Campillo Ricardi <dcampillo05@gmail.com>
2024-01-06 15:27:45 -0300

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,65 @@
+// Menu fixo
 window.addEventListener("scroll", function(){
     let header = document.querySelector('#header')
 
     header.classList.toggle('roll', window.scrollY > 0);
+})
+
+// Efeito rolagem
+window.revelar = ScrollReveal({reset:true})
+
+// TOP Site
+
+revelar.reveal('.efeito-txt-top', {
+    duration: 2000,
+    distance: '90px'
+})
+
+revelar.reveal('.efeito-img-top', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+// titulo 
+
+revelar.reveal('.titulo', {
+    duration: 2000,
+    distance: '90px'
+})
+
+// Habilidades
+
+revelar.reveal('.effect-hab1', {
+    duration: 2000,
+    distance: '90px',
+    delay: 500
+})
+
+revelar.reveal('.effect-hab2', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1000
+})
+
+revelar.reveal('.effect-hab3', {
+    duration: 2000,
+    distance: '90px',
+    delay: 1500
+})
+
+// Sobre
+
+revelar.reveal('.effect-txt-sobre', {
+    duration: 2000,
+    distance: '90px',
+    origin: 'right',
+    delay: 700
+})
+
+revelar.reveal('.effect-img-sobre', {
+    duration: 2000,
+    distance: '90px',
+    delay: 700,
+    origin: 'left' 
 })
